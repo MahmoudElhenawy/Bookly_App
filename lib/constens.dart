cb5a0b78ddaa2cb0b9e3974cf6d2637d7ac58021
MahmoudElhenawy <mahmoud202203355@gmail.com>
2025-06-12 16:05:38 +0300

    first commit

---
+++ lib/constens.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+
+const kpcolor = Color(0xff100B20);
+const kTransationDuration = Duration(milliseconds: 350);
---
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -0,0 +1,31 @@
+import 'package:bookly/core/utils/styles.dart';
+import 'package:bookly/features/home/presentation/views/widgets/custom_app_bar.dart';
+import 'package:bookly/features/home/presentation/views/widgets/featured_books_list_view.dart';
+import 'package:flutter/material.dart';
+
+class HomeViewBody extends StatelessWidget {
+  const HomeViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(
+        horizontal: 16,
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          CustomAppBar(),
+          FeaturedBooksListView(),
+          SizedBox(
+            height: 40,
+          ),
+          Text(
+            '  Best Seller',
+            style: Styles.textMeduiem,
+          ),
+        ],
+      ),
+    );
+  }
+}
