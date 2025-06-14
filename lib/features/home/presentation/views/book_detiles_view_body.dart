a17a5c16e861ca941e0c27c9cb34253e1f6ccf80
MahmoudElhenawy <mahmoud202203355@gmail.com>
2025-06-15 02:00:30 +0300

    add custtom item

--- lib/features/home/presentation/views/book_detiles_view_body.dart
+++ lib/features/home/presentation/views/book_detiles_view_body.dart
@@ -1,3 +1,5 @@
+import 'package:bookly/features/home/presentation/views/widgets/custom_list_view_item.dart';
+import 'package:bookly/features/home/presentation/views/widgets/custtom_book_detiles_app_bar.dart';
 import 'package:flutter/material.dart';
 
 class BookDetilesViewBody extends StatelessWidget {
@@ -5,28 +7,18 @@ class BookDetilesViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var Width = MediaQuery.of(context).size.width;
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 30),
       child: Column(
         children: [
           CusttomBoohDetilesAppBar(),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: Width * 0.17),
+            child: CustomListViewItem(),
+          ),
         ],
       ),
-    );
-  }
-}
-
-class CusttomBoohDetilesAppBar extends StatelessWidget {
-  const CusttomBoohDetilesAppBar({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      children: [
-        IconButton(onPressed: () {}, icon: Icon(Icons.close)),
-        Spacer(),
-        IconButton(onPressed: () {}, icon: Icon(Icons.shopping_cart_outlined)),
-      ],
     );
   }
 }
---
+++ lib/features/home/presentation/views/widgets/custtom_book_detiles_app_bar.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class CusttomBoohDetilesAppBar extends StatelessWidget {
+  const CusttomBoohDetilesAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        IconButton(onPressed: () {}, icon: Icon(Icons.close)),
+        Spacer(),
+        IconButton(onPressed: () {}, icon: Icon(Icons.shopping_cart_outlined)),
+      ],
+    );
+  }
+}
