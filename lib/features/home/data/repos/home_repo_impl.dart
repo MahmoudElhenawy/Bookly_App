4eeb188d69d98c207f2bc42b0287660849c1f83e
MahmoudElhenawy <mahmoud202203355@gmail.com>
2025-06-18 01:35:56 +0300

    implrment newset books

---
+++ lib/features/home/data/repos/home_repo_impl.dart
@@ -0,0 +1,37 @@
+import 'package:bookly/core/errors/failuers.dart';
+import 'package:bookly/core/utils/api_service.dart';
+import 'package:bookly/features/home/data/models/book_model/book_model.dart';
+import 'package:bookly/features/home/data/repos/home_repo.dart';
+import 'package:dartz/dartz.dart';
+
+class HomeRepoImpl implements HomeRepo {
+  final ApiService apiService;
+
+  HomeRepoImpl(this.apiService);
+  @override
+  Future<Either<Failuers, List<BookModel>>> fetchNewsetBooks() async {
+    try {
+      var data = await apiService.get(
+          endpoint:
+              'volumes?Filtering=free-ebooks&Sorting=newest &q=computer science');
+      List<BookModel> books = [];
+      for (var item in data['items']) {
+        try {
+          books.add(BookModel.fromJson(item));
+        } catch (e) {
+          books.add(BookModel.fromJson(item));
+        }
+      }
+
+      return right(books);
+    } catch (e) {
+      return left(serverFailuers());
+    }
+  }
+
+  @override
+  Future<Either<Failuers, List<BookModel>>> fetchFeaturedBooks() {
+    // TODO: implement fetchFeaturedBooks
+    throw UnimplementedError();
+  }
+}
